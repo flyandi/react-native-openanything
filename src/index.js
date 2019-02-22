d1de96116c1d4a3a5fe5bb27d08cbc80d36d14fc
Wesley de Faveri <faveriwesley@gmail.com>
2019-02-22 10:38:53 -0300

    export pdf

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 /**
  * react-native-openanything
  */
-import {Call} from './call';
-import {Email} from './email';
-import {Text} from './text';
-import {Web} from './web';
-import {Map} from './map';
-import {Launch, Supported, Open} from './launcher';
-import {Twitter} from './twitter'
-import {Youtube} from './youtube';
-import {Facetime} from './facetime';
-import {Facebook} from './facebook';
-import {Instagram, InstagramMedia, InstagramApp, InstagramCamera, InstagramTag, InstagramLocation} from './instagram';
+import { Call } from './call';
+import { Email } from './email';
+import { Text } from './text';
+import { Web } from './web';
+import { Map } from './map';
+import { Launch, Supported, Open } from './launcher';
+import { Twitter } from './twitter'
+import { Youtube } from './youtube';
+import { Facetime } from './facetime';
+import { Facebook } from './facebook';
+import { Instagram, InstagramMedia, InstagramApp, InstagramCamera, InstagramTag, InstagramLocation } from './instagram';
+import { Pdf } from './pdf';
 
-export {Call, Email, Text, Web, Launch, Supported, Open, Map, Twitter, Youtube, Facetime, Facebook, Instagram, InstagramMedia, InstagramApp, InstagramCamera, InstagramTag, InstagramLocation}
+export { Call, Email, Text, Web, Launch, Supported, Open, Map, Twitter, Youtube, Facetime, Facebook, Instagram, InstagramMedia, InstagramApp, InstagramCamera, InstagramTag, InstagramLocation, Pdf }
